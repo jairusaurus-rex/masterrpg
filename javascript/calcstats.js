fe8b918fc1175dfd2449e4be8ae48a488cf39909
jairusaurus-rex <75216239+jairusaurus-rex@users.noreply.github.com>
2020-12-09 08:31:38 -0600

    Calc can alter Magic Stats

--- javascript/calcstats.js
+++ javascript/calcstats.js
@@ -12,6 +12,8 @@ var aura = 0;
 var ability_points = 0;
 var reserve_points = 0;
 
+var magic_spell_tiers = 0;
+var magical_source = 0;
 function changeEverything(){
 	strength = parseInt($("#strength_input").val())
 	dexterity = parseInt($("#dexterity_input").val())
@@ -25,8 +27,31 @@ function changeEverything(){
 	bonus = Math.floor(2+ (level /5))
 	$("#bonus_display").text(bonus);
 	ability_points = 20 + level + bonus;
-	console.log(ability_points)
+	magic_spell_tiers = parseInt($("#spells_input").val())
 	
+	switch($("#source_input").val()){
+	case "none":
+		magical_source = 0;
+		break;
+	case "Totem":
+		magical_source = grit;
+		break;
+	case "Familiar":
+		magical_source = _focus;
+		break;
+	case "Spellbook":
+		magical_source = intelligence;
+		break;
+	case "Pact":
+		magical_source = wit;
+		break;
+	case "Bloodline":
+		magical_source = charisma;
+		break;
+	case "Prodigy":
+		magical_source = aura;
+		break;
+	}
 	
 	reserve_points = ability_points - calc_spent(strength) - calc_spent(dexterity) - calc_spent(grit) - calc_spent(_focus) - calc_spent(intelligence) - calc_spent(wit) - calc_spent(charisma) - calc_spent(aura) ;
 	$("#reserve_display").text(reserve_points);
@@ -37,9 +62,10 @@ function changeEverything(){
 	$("#large_display").text((strength * 2) + grit + bonus);
 	$("#small_display").text(bonus);
 	$("#wealth_display").text(level * bonus * 50 + 10 * (intelligence+wit+charisma));
-	$("#spells_known_display").text(intelligence + bonus);
-	$("#spells_mana_display").text(aura + bonus);
-	$("#worthy_display").text(aura + bonus);
+	$("#spells_known_display").text(intelligence + bonus + magic_spell_tiers + magical_source);
+	$("#spells_mana_display").text(aura + bonus + magic_spell_tiers + magical_source);
+	$("#recovery_mana_display").text(grit + bonus + magical_source);
+	$("#worthy_display").text(aura + bonus + magical_source);
 	$("#proficiencies_display").text(5 + bonus + charisma + intelligence + wit);
 	$("#feat_display").text(level*2 + bonus);
 }
